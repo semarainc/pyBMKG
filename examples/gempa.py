751a8061afe1b9b8ceab8380840ab9b5f2c1574b
Semara Incorporated <semaraincorporated@gmail.com>
2018-08-18 23:17:41 +0800

    Update gempa.py
    Fix Some Wrong Algorythm

--- examples/gempa.py
+++ examples/gempa.py
@@ -1,6 +1,7 @@
 import math
 from pybmkg import PyBMKGApi as gempa
 import time
+import requests
 
 #For Count Distance Between To Coordinates (6371 adalah radius bumi dalam KM)
 def haversine(latitude_awal,longitude_awal,latitude_kedua,longitude_kedua, R=6371):
@@ -56,7 +57,7 @@ def main():
 			if str(tanggal) != str(now["tanggal"]):
 
 				bali_terdampak = "Null"
-				if ('denpasar' in str(temp["dirasakan"]).lower()) or ('bali' in str(temp["dirasakan"]).lower()):
+				if ('denpasar' in str(now["dirasakan"]).lower()) or ('bali' in str(now["dirasakan"]).lower()):
 					bali_terdampak = "Ya"
 				else:
 					bali_terdampak = "Tidak"
@@ -66,20 +67,25 @@ def main():
 				longitude_gempa = float(temp_coord[0])
 
 				print("\n\n[NEWS].................INFO GEMPA...............[NEWS]")
-				print("\tTanggal: %s" % (temp["tanggal"]))
-				print("\tKoordinat: %s Posisi: %s" % (temp["koordinat"], temp["posisi"]))
-				print("\tMagnitude: %s SR(Skala Ritcher)" % (temp["magnitude"]))
-				print("\tKedalaman: %s" % (temp["kedalaman"]))
-				print("\tKeterangan: \n\t%s\n" % (temp["keterangan"]))
-				print("\tDirasakan: \n\t%s\n" % (temp["dirasakan"]))
+				print("\tTanggal: %s" % (now["tanggal"]))
+				print("\tKoordinat: %s Posisi: %s" % (now["koordinat"], now["posisi"]))
+				print("\tMagnitude: %s SR(Skala Ritcher)" % (now["magnitude"]))
+				print("\tKedalaman: %s" % (now["kedalaman"]))
+				print("\tKeterangan: \n\t%s\n" % (now["keterangan"]))
+				print("\tDirasakan: \n\t%s\n" % (now["dirasakan"]))
 				print("\tJarak Dari Bali (Denpasar): %s KM" % (haversine(latitude_bali, longitude_bali, latitude_gempa, longitude_gempa)) )
 				print("\tPerkiraan Bali Terkena Dampak: %s" % (bali_terdampak))
 				print("[News]..................INFO GEMPA...............[NEWS]\n\n")
+				if bali_terdampak.lower() == "ya":
+					print("---------------------------- WARNING GEMPA TERDETEKSI ----------------------------")
 				print("Waiting For Newest EarthQuake Info....")
+				tanggal = now["tanggal"]
 
-	except Exception as e:
+	except requests.exceptions.RequestException as e:
 		print("[Error] Detected Error")
+		print("Connection Problem Detected ReRun App")
 		print(e)
+		main()
 
 if __name__ == "__main__":
 	main()
