69e3efd1f00a37d24645217350ab34cb2285d573
semarainc <semaraincorporated@gmail.com>
2018-08-20 00:49:32 +0800

    Updated to 0.4 Alpha
    i updated the module to 0.4 Alpha
    
    Changes:
    * Fixing EarthQuake Sync With Central BMKG
    * Added Proxy Support
    * Fix Some Minor Bug on example and module

--- examples/gempa.py
+++ examples/gempa.py
@@ -1,6 +1,7 @@
 import math
-from pybmkg import PyBMKGApi as gempa
+from pybmkg import PyBMKGApi as BMKGAPI
 import time
+import requests
 
 #For Count Distance Between To Coordinates (6371 adalah radius bumi dalam KM)
 def haversine(latitude_awal,longitude_awal,latitude_kedua,longitude_kedua, R=6371):
@@ -17,6 +18,7 @@ def haversine(latitude_awal,longitude_awal,latitude_kedua,longitude_kedua, R=637
 
 def main():
 
+	gempa = BMKGAPI.EarthQuake('36.89.106.25:8080')
 	#Latitude And Longitude Bali
 	latitude_bali = float(115.2167)
 	longitude_bali = float(-8.650000001)
@@ -26,26 +28,27 @@ def main():
 	print("[DEBUG] Getting Latest EarthQuake News")
 	try:
 		temp = gempa.GetLatestEarthQuake()
-		tanggal = str(temp["tanggal"])
+		tanggal = str(temp["jam"])
+
+		#dalam BMKG cara baca koordinatnya (longitude, latitude)  ---> kecuali untuk gempa terbaru/terkini pembacaannya latitude,longitude
+		temp_coord = str(temp["koordinat"]).split(',')
+		latitude_gempa = float(temp_coord[0])
+		longitude_gempa = float(temp_coord[1])
 
 		bali_terdampak = "Null"
-		if ('denpasar' in str(temp["dirasakan"]).lower()) or ('bali' in str(temp["dirasakan"]).lower()):
+		if (float(temp["magnitude"]) >= 5.0) and (haversine(latitude_bali, longitude_bali, latitude_gempa, longitude_gempa) <= 200)  :
 			bali_terdampak = "Ya"
 		else:
 			bali_terdampak = "Tidak"
 
-		#dalam BMKG cara baca koordinatnya (longitude, latitude)
-		temp_coord = str(temp["koordinat"]).split(', ')
-		latitude_gempa = float(temp_coord[1])
-		longitude_gempa = float(temp_coord[0])
 
 		print("\n\n[NEWS].................INFO GEMPA...............[NEWS]\n")
-		print("\tTanggal: %s" % (temp["tanggal"]))
+		print("\tTanggal: %s %s" % (temp["tanggal"],temp["jam"]))
 		print("\tKoordinat: %s Posisi: %s" % (temp["koordinat"], temp["posisi"]))
 		print("\tMagnitude: %s SR(Skala Ritcher)" % (temp["magnitude"]))
 		print("\tKedalaman: %s" % (temp["kedalaman"]))
-		print("\tKeterangan: \n\t%s\n" % (temp["keterangan"]))
-		print("\tDirasakan: \n\t%s\n" % (temp["dirasakan"]))
+		print("\tKeterangan: \n\t%s\n" % (temp["wilayah"]))
+		#print("\tDirasakan: \n\t%s\n" % (temp["dirasakan"]))
 		print("\tJarak Dari Bali (Denpasar): %s KM" % (haversine(latitude_bali, longitude_bali, latitude_gempa, longitude_gempa)) )
 		print("\tPerkiraan Bali Terkena Dampak: %s" % (bali_terdampak))
 		print("\n[News]..................INFO GEMPA...............[NEWS]\n\n")
@@ -53,33 +56,39 @@ def main():
 		while True:
 			now = gempa.GetLatestEarthQuake()
 			time.sleep(5)
-			if str(tanggal) != str(now["tanggal"]):
+			if str(tanggal) != str(now["jam"]):
+
+				temp_coord = str(now["koordinat"]).split(',')
+				latitude_gempa = float(temp_coord[0])
+				longitude_gempa = float(temp_coord[1])
 
 				bali_terdampak = "Null"
-				if ('denpasar' in str(temp["dirasakan"]).lower()) or ('bali' in str(temp["dirasakan"]).lower()):
+				if (float(now["magnitude"]) >= 5.0) and (haversine(latitude_bali, longitude_bali, latitude_gempa, longitude_gempa) <= 200)  :
 					bali_terdampak = "Ya"
 				else:
 					bali_terdampak = "Tidak"
 
-				temp_coord = str(now["koordinat"]).split(', ')
-				latitude_gempa = float(temp_coord[1])
-				longitude_gempa = float(temp_coord[0])
 
 				print("\n\n[NEWS].................INFO GEMPA...............[NEWS]")
-				print("\tTanggal: %s" % (temp["tanggal"]))
-				print("\tKoordinat: %s Posisi: %s" % (temp["koordinat"], temp["posisi"]))
-				print("\tMagnitude: %s SR(Skala Ritcher)" % (temp["magnitude"]))
-				print("\tKedalaman: %s" % (temp["kedalaman"]))
-				print("\tKeterangan: \n\t%s\n" % (temp["keterangan"]))
-				print("\tDirasakan: \n\t%s\n" % (temp["dirasakan"]))
+				print("\tTanggal: %s %s" % (now["tanggal"],now["jam"]))
+				print("\tKoordinat: %s Posisi: %s" % (now["koordinat"], now["posisi"]))
+				print("\tMagnitude: %s SR(Skala Ritcher)" % (now["magnitude"]))
+				print("\tKedalaman: %s" % (now["kedalaman"]))
+				print("\tKeterangan: \n\t%s\n" % (now["wilayah"]))
+				#print("\tDirasakan: \n\t%s\n" % (now["dirasakan"]))
 				print("\tJarak Dari Bali (Denpasar): %s KM" % (haversine(latitude_bali, longitude_bali, latitude_gempa, longitude_gempa)) )
 				print("\tPerkiraan Bali Terkena Dampak: %s" % (bali_terdampak))
 				print("[News]..................INFO GEMPA...............[NEWS]\n\n")
+				if bali_terdampak.lower() == "ya":
+					print("---------------------------- WARNING GEMPA TERDETEKSI ----------------------------")
 				print("Waiting For Newest EarthQuake Info....")
+				tanggal = now["jam"]
 
-	except Exception as e:
+	except requests.exceptions.RequestException as e:
 		print("[Error] Detected Error")
+		print("Connection Problem Detected ReRun App")
 		print(e)
+		main()
 
 if __name__ == "__main__":
 	main()
--- examples/pybmkg/PyBMKGApi.py
+++ examples/pybmkg/PyBMKGApi.py
@@ -1,45 +1,84 @@
 from bs4 import BeautifulSoup as soup
 from requests import get
 
+
+
 #I'm currently trying to detect only the earthquake right now cuzz my area often get earthquake so i want to make early notification for my server to notify me at home ^_^
+class EarthQuake:
+	def __init__(self,proxy=None):
+		if proxy == None:
+			self.proxy = proxy
+		else:
+			self.proxy = {
+						"http" : proxy,
+						"https" : proxy
+					}
 
-def GetLatestEarthQuake():
-	data_raw = get('http://dataweb.bmkg.go.id/inatews/gempadirasakan.xml')
-	if data_raw.status_code != 200:
-		print("There Is Problem With Your Internet Connection or The Website Server is Down")
-	else:
-		soup_gempa = soup(data_raw.text, 'lxml')
-		data_gempa = soup_gempa.find('gempa')
-		dict_gempa = {
-			"tanggal" : data_gempa.find('tanggal').get_text(),
-			"koordinat" : data_gempa.find('coordinates').get_text(),
-			"posisi" : data_gempa.find('posisi').get_text(),
-			"magnitude" : data_gempa.find('magnitude').get_text(),
-			"kedalaman" : data_gempa.find('kedalaman').get_text(),
-			"keterangan" : str(data_gempa.find('keterangan').get_text()).replace("\t",' '),
-			"dirasakan" : str(data_gempa.find('dirasakan').get_text()).replace("\t",' ')
+		self.headers = {
+				'Connection': 'keep-alive', 
+				'Accept': 'application/json, text/plain, */*', 
+				'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G955N Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36',
+				'Accept-Language': 'en-ID,en-US;q=0.8',
+				'X-Requested-With': 'com.Info_BMKG',
+				'Cache-Control' : 'no-cache'
 		}
-	return dict_gempa
 
-def GetListEarthQuake():
-	dict_list_gempa = {}
-	data_raw = get('http://dataweb.bmkg.go.id/inatews/gempadirasakan.xml')
-	if data_raw.status_code != 200:
-		print("There Is Problem With Your Internet Connection or The Website Server is Down")
-	else:
-		soup_gempa = soup(data_raw.text, 'lxml')
-		data_gempa = soup_gempa.find_all('gempa')
-
-		for i in range(len(data_gempa)):
-
-			dict_list_gempa[i] = {
-				"tanggal" : data_gempa[i].find('tanggal').get_text(),
-				"koordinat" : data_gempa[i].find('coordinates').get_text(),
-				"posisi" : data_gempa[i].find('posisi').get_text(),
-				"magnitude" : data_gempa[i].find('magnitude').get_text(),
-				"kedalaman" : data_gempa[i].find('kedalaman').get_text(),
-				"keterangan" : data_gempa[i].find('keterangan').get_text(),
-				"dirasakan" : data_gempa[i].find('dirasakan').get_text()
+	def GetLatestEarthQuake(self):
+		data_raw = get('http://dataweb.bmkg.go.id/inatews/gempaterkini.xml?decache=0.755139865912497',headers=self.headers,proxies=self.proxy)
+		if data_raw.status_code != 200:
+			print("There Is Problem With Your Internet Connection or The Website Server is Down")
+		else:
+			soup_gempa = soup(data_raw.text, 'lxml')
+			data_gempa = soup_gempa.find('gempa')
+			dict_latest_gempa = {
+				"tanggal" : data_gempa.find('tanggal').get_text(),
+				"jam" : data_gempa.find('jam').get_text(),
+				"koordinat" : data_gempa.find('coordinates').get_text(),
+				"posisi" : data_gempa.find('lintang').get_text() + ' '+ data_gempa.find('bujur').get_text(),
+				"magnitude" : float(data_gempa.find('magnitude').get_text().replace('SR','')),
+				"kedalaman" : data_gempa.find('kedalaman').get_text(),
+				"wilayah" : str(data_gempa.find('wilayah').get_text()).replace("\t",' ')
 			}
+		return dict_latest_gempa
 
-	return dict_list_gempa
+	#i just realized that this method is slower than GetLatestEarthQuake, so this is optional if you want to get more info like perceived earthquake :v
+	def GetPerceivedEarthQuake(self):
+		data_raw = get('http://dataweb.bmkg.go.id/inatews/gempadirasakan.xml?decache=0.755139865912497',headers=self.headers,proxies=self.proxy)
+		if data_raw.status_code != 200:
+			print("There Is Problem With Your Internet Connection or The Website Server is Down")
+		else:
+			soup_gempa = soup(data_raw.text, 'lxml')
+			data_gempa = soup_gempa.find('gempa')
+			dict_gempa = {
+				"tanggal" : data_gempa.find('tanggal').get_text(),
+				"koordinat" : data_gempa.find('coordinates').get_text(),
+				"posisi" : data_gempa.find('posisi').get_text(),
+				"magnitude" : data_gempa.find('magnitude').get_text(),
+				"kedalaman" : data_gempa.find('kedalaman').get_text(),
+				"keterangan" : str(data_gempa.find('keterangan').get_text()).replace("\t",' '),
+				"dirasakan" : str(data_gempa.find('dirasakan').get_text()).replace("\t",' ')
+			}
+		return dict_gempa
+
+	def GetListEarthQuake(self):
+		dict_list_gempa = {}
+		data_raw = get('http://dataweb.bmkg.go.id/inatews/gempadirasakan.xml?decache=0.755139865912497', headers=self.headers, proxies=self.proxy)
+		if data_raw.status_code != 200:
+			print("There Is Problem With Your Internet Connection or The Website Server is Down")
+		else:
+			soup_gempa = soup(data_raw.text, 'lxml')
+			data_gempa = soup_gempa.find_all('gempa')
+
+			for i in range(len(data_gempa)):
+
+				dict_list_gempa[i] = {
+					"tanggal" : data_gempa[i].find('tanggal').get_text(),
+					"koordinat" : data_gempa[i].find('coordinates').get_text(),
+					"posisi" : data_gempa[i].find('posisi').get_text(),
+					"magnitude" : data_gempa[i].find('magnitude').get_text(),
+					"kedalaman" : data_gempa[i].find('kedalaman').get_text(),
+					"keterangan" : data_gempa[i].find('keterangan').get_text(),
+					"dirasakan" : data_gempa[i].find('dirasakan').get_text()
+				}
+
+		return dict_list_gempa
